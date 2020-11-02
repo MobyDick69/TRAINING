e01c20bb81ff4a724f2aa3c05f7f972814909dd9
Andrea Alessandrini <andrea.alessandrini1992@gmail.com>
2020-11-02 17:08:45 +0100

    Added .gitignore file and Commit of prova_statistics_training.py

---
+++ prova_statistics_training.py
@@ -0,0 +1,13 @@
+######################################################
+# Program to collect and analyse data from trainings #
+######################################################
+
+
+######################TEST CODE######################
+exer = "4x15w10.0"
+
+for symbol in exer:
+	if symbol == "w":
+		print("L'esercizio è stato eseguito con pesi!")
+
+#####################################################
