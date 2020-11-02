7496995258d370b8a275dd0a7c8bbbbd9bff2023
Andrea Alessandrini <andrea.alessandrini1992@gmail.com>
2020-11-02 18:52:15 +0100

    Updated prova_statistics_training-py

--- prova_statistics_training.py
+++ prova_statistics_training.py
@@ -1,13 +1,16 @@
 ######################################################
 # Program to collect and analyse data from trainings #
 ######################################################
-
+import numpy as np
 
 ######################TEST CODE######################
 exer = "4x15w10.0"
 
 for symbol in exer:
 	if symbol == "w":
-		print("L'esercizio è stato eseguito con pesi!")
-
+		print("\nL'esercizio è stato eseguito con pesi!\n")
+		series_n = exer[0]
+		rep_n    = exer[2:4]
+		weight   = exer[-4:]
+print(f"Sono state eseguite {series_n} serie da {rep_n} ripetizioni,\nutilizzando un peso da {weight} kg.\n")
 #####################################################
