afa9c8dd1672ac64bc7ffdfccf6197b5cdb4a2cc
Andrea Alessandrini <andrea.alessandrini1992@gmail.com>
2020-11-03 18:38:19 +0100

    Update program: convert char var to num var

--- prova_statistics_training.py
+++ prova_statistics_training.py
@@ -3,14 +3,17 @@
 ######################################################
 import numpy as np
 
-######################TEST CODE######################
+###################################TEST CODE###################################
+# N.B. int() converte var char in variabili numeriche.
 exer = "4x15w10.0"
 
 for symbol in exer:
 	if symbol == "w":
 		print("\nL'esercizio è stato eseguito con pesi!\n")
-		series_n = exer[0]
-		rep_n    = exer[2:4]
+		series_n = int(exer[0])
+		rep_n    = int(exer[2:4])
 		weight   = exer[-4:]
-print(f"Sono state eseguite {series_n} serie da {rep_n} ripetizioni,\nutilizzando un peso da {weight} kg.\n")
-#####################################################
+print(f"Sono state eseguite {series_n} serie da {rep_n} ripetizioni, "
+	  f"utilizzando un peso da {weight} kg.")
+print(type(series_n))
+###############################################################################
